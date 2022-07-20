75b3bef6d49bae5d1c0aeb7a60ec06f488cc2c49
deividbreda <deivid.breda02@gmail.com>
2022-07-20 01:37:39 -0300

    Criando component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+export function Footer(){
+    return(
+        <span 
+            style={{textAlign: "center", display: "block", margin: "0 0 64px 0", color: "#ccc;"}}> 
+            Desenvolvido por eu mesmo! 😜 
+        </span>
+    );
+}
