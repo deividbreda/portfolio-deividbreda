58708144add63b077ad3b55d4d1cd58bc5b82fa7
deividbreda <deivid.breda02@gmail.com>
2022-07-20 01:38:15 -0300

    Implementando AOS com useEffect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    Aos.init({duration: 1050})
+  }, [])
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ export default class MyDocument extends Document {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Poppins:wght@200;400;600;700&display=swap" rel="stylesheet" />
 
+                <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/62572750?v=4" type="image/png" />
+
                 </Head>
                 <body>
                     <Main />
