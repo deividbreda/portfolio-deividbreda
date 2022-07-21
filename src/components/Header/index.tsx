79f7e11a35be680e57b3650ba2b1ed8744730343
deividbreda <deivid.breda02@gmail.com>
2022-07-21 01:08:31 -0300

    Adicionando redes ao Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,29 @@ import { Links } from './Links';
 
 import styles from './styles.module.scss'
 
-export function Header(){
-    return(
+import { FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+
+export function Header() {
+    return (
         <div className={styles.container}>
             <div className="containerWidth">
                 <div className={styles.content}>
-                    <div data-aos="fade-right" className={styles.info}>                 
+                    <div data-aos="fade-right" className={styles.info}>
                         <img src="https://avatars.githubusercontent.com/u/62572750?v=4" alt="" />
                         <div className={styles.text}>
-                            <h1> Deivid Breda </h1>
-                            <p> Web Developer | React | Next | JS | GraphQL </p>
+                            <div>
+                                <h1> Deivid Breda </h1>
+                                <div className={styles.redes}>
+                                    <a href="https://www.linkedin.com/in/deivid-breda-9b2507136/" target="_blank"> <FaLinkedinIn /> </a>
+                                    <a href="https://web.whatsapp.com/send?phone=5542991062425" target="_blank"> <FaWhatsapp /> </a>
+                                </div>
+                            </div>
+
+                            <p> Web Developer | React | Next | TypeScript | JS | GraphQL </p>
+
                         </div>
                     </div>
-                    
+
                     <Links />
                 </div>
             </div>
