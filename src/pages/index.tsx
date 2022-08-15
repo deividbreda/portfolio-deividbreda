ce738ba4772d662c10677f39490c4bf640f16675
deividbreda <deivid.breda02@gmail.com>
2022-08-15 19:49:11 -0300

    Importando nova pasta dos componentes Portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import { useState } from 'react'
-import { Footer } from '../components/Footer'
-import { Header } from '../components/Header'
-import { Contatos } from '../components/Home/Contatos'
-import { GithubRepos } from '../components/Home/GithubRepositories'
-import { Servicos } from '../components/Home/Serviços'
-import { Sobre } from '../components/Home/Sobre'
-import { Tecnologias } from '../components/Home/Tecnologias'
-import { ModalSkills } from '../components/Home/Tecnologias/ModalSkills'
+
+import { Footer } from '../components/Portfolio/Footer'
+import { Header } from '../components/Portfolio/Header'
+import { Contatos } from '../components/Portfolio/Home/Contatos'
+import { GithubRepos } from '../components/Portfolio/Home/GithubRepositories'
+import { Servicos } from '../components/Portfolio/Home/Serviços'
+import { Sobre } from '../components/Portfolio/Home/Sobre'
+import { Tecnologias } from '../components/Portfolio/Home/Tecnologias'
+import { ModalSkills } from '../components/Portfolio/Home/Tecnologias/ModalSkills'
 
 export default function Home() {
   const [openModalSkills, setOpenModalSkills] = useState(false);
