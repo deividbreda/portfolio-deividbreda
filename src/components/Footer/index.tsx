13906f36b4bebe6c18a63632ff99581941c56297
deividbreda <deivid.breda02@gmail.com>
2022-08-15 19:40:30 -0300

    Movendo pasta Portfolio

--- src/components/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-export function Footer(){
-    return(
-        <span 
-            style={{textAlign: "center", display: "block", margin: "0 0 64px 0", color: "#ccc;"}}> 
-            Desenvolvido por eu mesmo! 😜 
-        </span>
-    );
-}
