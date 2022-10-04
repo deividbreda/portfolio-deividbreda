ee7a9858373752f7e90b1c6c21fb1b5f6c34b63b
deividbreda <deivid.breda02@gmail.com>
2022-10-04 11:04:51 -0300

    Criando page Cadastra

---
+++ src/pages/blog/cadastrar.tsx
@@ -0,0 +1,12 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function Cadastrar() {
+    return (
+        <>
+            <Head>
+                <title> Nova conta | Blog | Deivid Breda </title>
+            </Head>
+        </>
+    );
+}
