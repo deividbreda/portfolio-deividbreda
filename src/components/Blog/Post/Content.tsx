c193439dad439369f897fc9df73cf00ebb991cc3
deividbreda <deivid.breda02@gmail.com>
2022-10-07 19:40:09 -0300

    Arrumando Content do Post

--- src/components/Blog/Post/Content.tsx
+++ src/components/Blog/Post/Content.tsx
@@ -8,15 +8,6 @@ export function Content({ post }: PostProps) {
     return (
         <Box maxW="740px" mx="auto" px="20px" w="100%">
             <Stack p="64px 0">
-                <Text
-
-                    lineHeight={['28px', '36px']}
-                    fontSize={['14px', '16px']}
-                    color="#555"
-                    textAlign="justify"
-                >
-
-                </Text>
                 <Box className={styles.content}>
                     <MDXRemote {...post.source} />
                 </Box>
