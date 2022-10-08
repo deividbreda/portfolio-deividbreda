7237019620219bbfe37ec0078f1a325acf861563
deividbreda <deivid.breda02@gmail.com>
2022-10-08 18:02:02 -0300

    Criando rota para Webhook do Hygraph

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.query.secret !== process.env.HYGRAPH_TOKEN) {
+        return res.status(401).json({ message: "Token Inválido" });
+    }
+
+    if (!req.body) {
+        return res.status(422).json({ message: "Requisição Inválida" });
+    }
+
+    try {
+        await res.revalidate(path.join("/", req.body.data.slug));
+        return res.status(200).json({ revalidated: true });
+    } catch (err) {
+        return res.status(500).send("Erro de Validação");
+    }
+}
