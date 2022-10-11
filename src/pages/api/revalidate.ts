c73debb513f503c00ec919c313991a85f74cbbdb
deividbreda <deivid.breda02@gmail.com>
2022-10-11 00:44:24 -0300

    Testando Variação de Revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
     if (req.query.secret !== process.env.HYGRAPH_TOKEN) {
         return res.status(401).json({ message: "Erro: Token Inválido" });
     }
@@ -11,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     try {
-        await res.revalidate(path.join("/blog/", req.body.data.slug));
+        await res.revalidate(path.join("/", req.body.data.slug));
         return res.status(200).json({ revalidated: true });
     } catch (err) {
         return res.status(500).send("Erro de Validação");
